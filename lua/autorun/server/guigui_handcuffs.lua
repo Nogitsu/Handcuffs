a441f0e914304bfc53d3dda9f208dc508877f5a3
Nogitsu <nogitsu.9@gmail.com>
2017-02-19 19:18:06 +0100

    Update guigui_handcuffs.lua

--- lua/autorun/server/guigui_handcuffs.lua
+++ lua/autorun/server/guigui_handcuffs.lua
@@ -1,5 +1,22 @@
 hook.Add("PlayerSwitchWeapon", "guigui_Handcuffs", function(ply)
 	if ply:HasWeapon("guigui_handcuffed") then
-		timer.Simple(0, function() ply:SelectWeapon("guigui_handcuffed") end)
+	ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_L_UpperArm"), Angle(20, 8.8, 0))
+	ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_L_Forearm"), Angle(15, 0, 0))
+	ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_L_Hand"), Angle(0, 0, 75))
+	ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_R_Forearm"), Angle(-15, 0, 0))
+	ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_R_Hand"), Angle(0, 0, -75))
+	ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_R_UpperArm"), Angle(-20, 16.6, 0))
+	timer.Simple(0, function() ply:SelectWeapon("guigui_handcuffed") end)
+	end
+end)
+
+hook.Add("PostPlayerDeath", "ResetPlayer", function(ply)
+	if ply:HasWeapon("guigui_handcuffed") then
+		ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_L_UpperArm"), Angle(0, 0, 0))
+		ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_L_Forearm"), Angle(0, 0, 0))
+		ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_L_Hand"), Angle(0, 0, 0))
+		ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_R_Forearm"), Angle(0, 0, 0))
+		ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_R_Hand"), Angle(0, 0, 0))
+		ply:ManipulateBoneAngles(ply:LookupBone("ValveBiped.Bip01_R_UpperArm"), Angle(0, 0, 0))
 	end
 end)
